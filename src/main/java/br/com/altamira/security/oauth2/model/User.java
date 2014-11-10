6d77fe344c827b218f022c6dc589d846abe031cd
jyotirmayad <jyotirmayad@mindfiresolutions.com>
2014-11-10 15:48:17 +0530

    Added getters and setters

--- src/main/java/br/com/altamira/security/oauth2/model/User.java
+++ src/main/java/br/com/altamira/security/oauth2/model/User.java
@@ -6,6 +6,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+
 /**
 *
 * @author 
@@ -34,6 +35,47 @@ public class User extends Resource {
 	@Size(min = 3)
 	@Column(name = "PASSWORD")
 	private String password = "";
+
+    /**
+     *
+     */
+    public User() {
+        
+    }
+	
+	/**
+    *
+    * @param number
+    * @param customer
+    */
+   public User(Long id, String user) {
+       this.id = id;
+       this.user = user;
+   }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 
 }
